225f97e54132f5f5f297d73c70e5a9dc4b3367b1
Haruma HIRABAYASHI <hrm1810884@g.ecc.u-tokyo.ac.jp>
2024-05-16 19:24:27 +0900

    add: useContext

---
+++ src/components/common/states/globalStates.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import React, { FC, useState } from "react";
+
+import { DrawingContext } from "@/states/DrawingContext";
+import { canvasContext, Brush, HistoryItem } from "@/types";
+
+type Props = {
+    children: React.ReactNode;
+};
+
+export const GlobalStateProvider: FC<Props> = ({ children }) => {
+    const [canvasContext, setCanvasContext] = useState<canvasContext>(null);
+    const [zoom, setZoom] = useState<number>(0.5);
+    const [brush, setBrush] = useState<Brush>({ type: "PENCIL", width: 3, color: "#000000" });
+    const [history, setHistory] = useState<HistoryItem[]>([]);
+    const [currentHistoryIndex, setCurrentHistoryIndex] = useState<number>(0);
+
+    return (
+        <DrawingContext.Provider
+            value={{
+                canvasContext,
+                setCanvasContext,
+                zoom,
+                setZoom,
+                brush,
+                setBrush,
+                history,
+                setHistory,
+                currentHistoryIndex,
+                setCurrentHistoryIndex,
+            }}
+        >
+            {children}
+        </DrawingContext.Provider>
+    );
+};
