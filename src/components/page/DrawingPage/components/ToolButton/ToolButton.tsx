810ef4699e070dd05d63091ff7b7d324de1342a3
Haruma HIRABAYASHI <hrm1810884@g.ecc.u-tokyo.ac.jp>
2024-05-20 18:55:39 +0900

    add: disable boolean

--- src/components/page/DrawingPage/components/ToolButton/ToolButton.tsx
+++ src/components/page/DrawingPage/components/ToolButton/ToolButton.tsx
@@ -2,21 +2,22 @@ import React, { FC } from "react";
 
 type props = {
     icon: React.ReactElement;
-    onClick: React.MouseEventHandler<HTMLDivElement>;
+    onClick: React.MouseEventHandler<HTMLButtonElement>;
     isSelected?: boolean;
+    isDisabled?: boolean;
 };
 
 export const ToolButton: FC<props> = (props) => {
-    const { icon, onClick, isSelected } = props;
+    const { icon, onClick, isSelected, isDisabled } = props;
 
     return (
         <div className="relative">
-            <div onClick={onClick} className="relative">
+            <button onClick={onClick} className="relative" disabled={isDisabled}>
                 <span className={"peer " + (isSelected && "text-indigo-600")}>{icon}</span>
                 {isSelected && (
                     <div className="absolute h-1 w-5 bg-indigo-500 rounded-t-lg left-1/2 -translate-x-1/2 translate-y-1.5"></div>
                 )}
-            </div>
+            </button>
         </div>
     );
 };
