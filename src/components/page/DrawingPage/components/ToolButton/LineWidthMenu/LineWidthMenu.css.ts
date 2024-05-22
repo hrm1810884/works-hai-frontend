2cdb4fc9771525bb7a17b83d79aea8c41dec7b08
Haruma HIRABAYASHI <hrm1810884@g.ecc.u-tokyo.ac.jp>
2024-05-22 22:43:27 +0900

    refine: change modal to selector for line width

---
+++ src/components/page/DrawingPage/components/ToolButton/LineWidthMenu/LineWidthMenu.css.ts
@@ -0,0 +1,17 @@
+import { style } from "@vanilla-extract/css";
+
+export const widthBar = style({
+    display: "flex",
+    width: "16px",
+    backgroundColor: "#4b5563",
+});
+
+export const menuDropDownStyle = style({
+    display: "flex",
+    flexDirection: "column",
+});
+
+export const menuStyle = style({
+    display: "flex",
+    flexDirection: "column",
+});
---
+++ src/components/page/DrawingPage/components/ToolButton/LineWidthMenu/index.ts
@@ -0,0 +1,1 @@
+export { LineWidthMenu } from "./LineWidthMenu";
