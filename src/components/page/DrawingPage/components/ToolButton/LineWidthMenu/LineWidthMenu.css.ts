504e7e5877c89eceb4fe9723d23836acc7d980ea
Haruma HIRABAYASHI <hrm1810884@g.ecc.u-tokyo.ac.jp>
2024-05-23 14:06:37 +0900

    hotfix: line width handler

--- src/components/page/DrawingPage/components/ToolButton/LineWidthMenu/LineWidthMenu.css.ts
+++ src/components/page/DrawingPage/components/ToolButton/LineWidthMenu/LineWidthMenu.css.ts
@@ -1,5 +1,7 @@
 import { style } from "@vanilla-extract/css";
 
+import { vars } from "@/styles";
+
 export const widthBar = style({
     display: "flex",
     width: "16px",
@@ -14,4 +16,8 @@ export const menuDropDownStyle = style({
 export const menuStyle = style({
     display: "flex",
     flexDirection: "column",
+});
+
+export const selectedStyle = style({
+    backgroundColor: vars.colors.primary[2],
 });
