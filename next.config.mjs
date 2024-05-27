72c75ff407bd60c735e6043e18278c6c90df24d4
Haruma HIRABAYASHI <hrm1810884@g.ecc.u-tokyo.ac.jp>
2024-05-27 22:37:00 +0900

    format: lint

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,21 @@ import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
 const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
+    },
+    reactStrictMode: true,
+    images: {
+        // FIXME: テスト用の設定追加なので、本番環境では削除する
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "placehold.jp",
+                port: "",
+            },
+        ],
+    },
+};
 
 export default withVanillaExtract(nextConfig);
