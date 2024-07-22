ac36e344479501e1d6e33d5c729c9b378d80ebce
Haruma HIRABAYASHI <hrm1810884@g.ecc.u-tokyo.ac.jp>
2024-07-23 03:11:19 +0900

    update tool hooks

--- src/components/page/DrawingPage/components/GenerationButton/GenerationButton.tsx
+++ src/components/page/DrawingPage/components/GenerationButton/GenerationButton.tsx
@@ -1,27 +1,26 @@
 import { ActionIcon } from "@mantine/core";
+import { FC } from "react";
 import { IoMdSend } from "react-icons/io";
-
-import { useGenerationButton } from "./hooks";
 
 import { vars } from "@/styles";
 
 import { sendButtonStyle } from "./GenerationButton.css";
 
-export const GenerationButton = () => {
-    const {
-        handler: { handleSubmit },
-    } = useGenerationButton();
-
+type props = {
+    text: string;
+    onClick: () => void;
+};
+export const GenerationButton: FC<props> = ({ text, onClick: handleClick }) => {
     return (
         <ActionIcon
             type="submit"
             variant="filled"
             color={vars.colors.white}
             radius={vars.radius.lg}
-            onClick={handleSubmit}
+            onClick={handleClick}
             className={sendButtonStyle}
         >
-            <span style={{ marginRight: vars.spacing.sm }}>{"AI生成"}</span>
+            <span style={{ marginRight: vars.spacing.sm }}>{text}</span>
             <IoMdSend style={{ display: "flex" }}></IoMdSend>
         </ActionIcon>
     );
