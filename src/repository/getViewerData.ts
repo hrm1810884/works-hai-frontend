ecbf9eb40d5bf3b4ca0fabc16e71c5d980e25f9c
Haruma HIRABAYASHI <hrm1810884@g.ecc.u-tokyo.ac.jp>
2024-07-25 19:06:50 +0900

    repo base ok

---
+++ src/repository/getViewerData.ts
@@ -0,0 +1,37 @@
+import { getGetViewQueryKey, useGetViewSuspense } from "@/generated/api";
+import { GetViewBody } from "@/generated/model";
+
+import { useQueryClient } from "@tanstack/react-query";
+import { useCallback } from "react";
+
+import { Position } from "@/model";
+
+export const useViwerRepository = (position: Position) => {
+    const queryClient = useQueryClient();
+
+    const req: GetViewBody = { position: { x: position.x, y: position.y } };
+
+    const {
+        data: {
+            data: { result: viewerData },
+        },
+    } = useGetViewSuspense(req);
+
+    const refetch = useCallback(async () => {
+        const refetchReq: GetViewBody = { position: { x: position.x, y: position.y } };
+        queryClient.invalidateQueries({ queryKey: getGetViewQueryKey(refetchReq) });
+    }, [queryClient, position]);
+
+    const url = viewerData.url;
+    const posX = viewerData.position.x;
+    const posY = viewerData.position.y;
+    const resPosition: Position = { x: posX, y: posY };
+
+    return {
+        data: {
+            url,
+            position: resPosition,
+        },
+        refetch,
+    };
+};
