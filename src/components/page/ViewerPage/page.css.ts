a59d93b31bf236c052692c9782de56db6ecf33c1
Haruma HIRABAYASHI <hrm1810884@g.ecc.u-tokyo.ac.jp>
2024-07-27 10:55:27 +0900

    pos ok

--- src/components/page/ViewerPage/page.css.ts
+++ src/components/page/ViewerPage/page.css.ts
@@ -2,9 +2,17 @@ import { style } from "@vanilla-extract/css";
 
 export const wrapper = style({
     display: "flex",
-    overflow: "scroll",
+    flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
     width: "100vw",
     height: "100vh",
+});
+
+export const controlWrapper = style({
+    display: "flex",
+    flexDirection: "row",
+    position: "absolute",
+    bottom: "5px",
+    zIndex: 100,
 });
--- src/components/page/ViewerPage/page.tsx
+++ src/components/page/ViewerPage/page.tsx
@@ -5,23 +5,24 @@ import { useViewer } from "./hooks";
 
 import { ViewerCard, ViewerGrid } from "./components";
 
+import { controlWrapper, wrapper } from "./page.css";
+
 export const ViewerPage = () => {
     const { data } = useViewer();
 
     const Control = () => {
         const { zoomIn, zoomOut, resetTransform } = useControls();
         return (
-            <>
+            <div className={controlWrapper}>
                 <button onClick={() => zoomIn()}>Zoom In</button>
                 <button onClick={() => zoomOut()}>Zoom Out</button>
                 <button onClick={() => resetTransform()}>Reset</button>
-            </>
+            </div>
         );
     };
 
     return (
-        // <div className={wrapper}>
-        <>
+        <div className={wrapper}>
             <TransformWrapper initialScale={1} initialPositionX={0} initialPositionY={0}>
                 <>
                     <Control />
@@ -39,7 +40,6 @@ export const ViewerPage = () => {
                     </TransformComponent>
                 </>
             </TransformWrapper>
-        </>
-        // </div>
+        </div>
     );
 };
