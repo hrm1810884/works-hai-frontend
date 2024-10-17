52262a5126a001ea1ada2b828b289a46dadf0c0d
nakahiro1206 <32neteru-56neteru-dullnoon@g.ecc.u-tokyo.ac.jp>
2024-10-17 22:06:03 +0900

    commit

--- src/components/page/DrawingPage/components/BottomToolBar/BottomToolBar.tsx
+++ src/components/page/DrawingPage/components/BottomToolBar/BottomToolBar.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react";
+import { IoMdSend } from "react-icons/io";
 
 import { useCanvas } from "@/states/Canvas";
+import { Now } from "@/utils/switchByEnv";
 
-import { GenerationButton } from "../GenerationButton";
+import { ButtonWithIcon } from "@/components/common/ui/";
 
-import { bottomToolBarStyle, toolButtonsContainerStyle, textSpanStyle } from "./BottomToolBar.css";
+import { bottomToolBarStyle, textSpanStyle, toolButtonsContainerStyle } from "./BottomToolBar.css";
 
 type Props = {
     onClick: () => void;
@@ -15,16 +17,39 @@ export const BottomToolBar: FC<Props> = (props) => {
 
     const { densityValidation } = useCanvas();
 
+    /**
+     * TODO: 開発が進み次第削除する
+     */
+    const enableValidation = false;
+
     return (
         <div className={bottomToolBarStyle}>
             <div className={toolButtonsContainerStyle}>
-                <div className={textSpanStyle}>
-                    <div>キャンバス周辺部分の余白の割合: {Math.round(densityValidation.whitePixelsProportion * 1000) / 10}%</div>
-                    <div>30%を下回ると完了ボタンが押せるようになります</div>
-                </div>
-                {densityValidation.validated ?
-                <GenerationButton text="完了" onClick={openModal} />
-                : null}
+                {/**
+                 * NOTE: ローカル環境ではバリデーションをストップ
+                 * enableする際にはenableValidationをtrueにする
+                 */}
+                {Now.isLocal && !enableValidation ? (
+                    <ButtonWithIcon type="submit" text="完了" icon={IoMdSend} onClick={openModal} />
+                ) : (
+                    <>
+                        <div className={textSpanStyle}>
+                            <div>
+                                キャンバス周辺部分の余白の割合:{" "}
+                                {Math.round(densityValidation.whitePixelsProportion * 1000) / 10}%
+                            </div>
+                            <div>30%を下回ると完了ボタンが押せるようになります</div>
+                        </div>
+                        {densityValidation.validated ? (
+                            <ButtonWithIcon
+                                type="submit"
+                                text="完了"
+                                icon={IoMdSend}
+                                onClick={openModal}
+                            />
+                        ) : null}
+                    </>
+                )}
             </div>
         </div>
     );
